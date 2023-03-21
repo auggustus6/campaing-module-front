b19a17510af3804f40d78b7043bbb06ca82390b0
ChrisCoy <christopherleegs@outlook.com>
2023-03-21 09:36:33 -0300

    chore: update api url

--- src/services/api.ts
+++ src/services/api.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const api = axios.create({
   // baseURL: 'http://localhost:3333',
-  baseURL: 'https://campaign-back.onrender.com',
+  baseURL: 'https://api-campaign.onrender.com/',
   // withCredentials: true,
 });
 
