d0bf4caef741ca5a9a33f595611c97661328d303
ChrisCoy <christopherleegs@outlook.com>
2023-03-24 11:14:17 -0300

    chore: change api link to deploy

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:3333',
-  // baseURL: 'https://api-campaign.onrender.com/',
+  // baseURL: 'http://localhost:3333',
+  baseURL: 'https://api-campaign.onrender.com/',
   // withCredentials: true,
 });
 
