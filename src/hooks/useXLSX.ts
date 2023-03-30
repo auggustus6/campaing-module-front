f785fc95e46ead9876b9b533d050a3428c975194
ChrisCoy <christopherleegs@outlook.com>
2023-03-30 11:00:20 -0300

    chore: sheets raw data

--- src/hooks/useXLSX.ts
+++ src/hooks/useXLSX.ts
@@ -8,21 +8,24 @@ export function useXLSX() {
   const excelToJson = async (file?: File): Promise<ExcelToJsonType> => {
     try {
       const workbook = XLSX.read(await file?.arrayBuffer());
-      console.log(workbook);
 
       const sheet_name_list = workbook.SheetNames;
 
       const xlData = XLSX.utils.sheet_to_json(
         workbook.Sheets[sheet_name_list[0]],
-        { defval: '', blankrows: true, raw: false }
+        { defval: '', blankrows: true, raw: true }
       );
 
-      console.log({ xlData });
-
-      const filteredData = xlData.map((item: any) => {
+      let filteredData = xlData.map((item: any) => {
         const { __rowNum__, ...rest } = item;
-
         return rest;
+      });
+
+      filteredData = filteredData.filter((item: any) => {
+        if (Object.values(item).every((x) => x === null || x === '')) {
+          return false;
+        }
+        return true;
       });
 
       return { data: filteredData };
