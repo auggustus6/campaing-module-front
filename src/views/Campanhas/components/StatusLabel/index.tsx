6d5798762c9634c59cac333b43fbc13e640888bc
ChrisCoy <christopherleegs@outlook.com>
2023-07-18 12:47:01 -0300

    chore: add new table status

--- src/views/Campanhas/components/StatusLabel/index.tsx
+++ src/views/Campanhas/components/StatusLabel/index.tsx
@@ -5,57 +5,111 @@ import DoneAllIcon from '@mui/icons-material/DoneAll';
 import PlayCircleIcon from '@mui/icons-material/PlayCircle';
 import SettingsEthernetIcon from '@mui/icons-material/SettingsEthernet';
 import ErrorIcon from '@mui/icons-material/Error';
+import { DoNotDisturb } from '@mui/icons-material';
 
 // function pick
 
 export default function StatusLabel({ status }: { status: string }) {
+  const commonStyles = {
+    display: 'flex',
+    gap: '4px',
+    alignItems: 'center',
+  };
+
   switch (status) {
     case 'CANCELADO':
       return (
-        <span style={{ color: '#ff7167', display: 'flex', gap: '4px' }}>
+        <span
+          style={{
+            ...commonStyles,
+            color: '#ff7167',
+          }}
+        >
           <CancelScheduleSendIcon />
           {status}
         </span>
       );
     case 'PAUSADO':
       return (
-        <span style={{ color: '#2cabff', display: 'flex', gap: '4px' }}>
+        <span
+          style={{
+            ...commonStyles,
+            color: '#2cabff',
+          }}
+        >
           <PauseCircleFilledIcon />
           {status}
         </span>
       );
     case 'EM_PROGRESSO':
       return (
-        <span style={{ color: '#3ac770', display: 'flex', gap: '4px' }}>
+        <span
+          style={{
+            ...commonStyles,
+            color: '#3ac770',
+          }}
+        >
           <ForwardToInboxIcon />
           {status}
         </span>
       );
     case 'CONCLUIDO':
       return (
-        <span style={{ color: '#2cabff', display: 'flex', gap: '4px' }}>
+        <span
+          style={{
+            ...commonStyles,
+            color: '#2cabff',
+          }}
+        >
           <DoneAllIcon />
           {status}
         </span>
       );
     case 'INICIAR':
       return (
-        <span style={{ color: '#e252f5', display: 'flex', gap: '4px' }}>
+        <span
+          style={{
+            ...commonStyles,
+            color: '#e252f5',
+          }}
+        >
           <PlayCircleIcon />
           {status}
         </span>
       );
     case 'PAUSE_AUTOMATIC':
       return (
-        <span style={{ color: '#9d21d6' }}>
+        <span
+          style={{
+            ...commonStyles,
+            color: '#9d21d6',
+          }}
+        >
           <SettingsEthernetIcon />
           {status}
         </span>
       );
     case 'ERRO':
       return (
-        <span style={{ color: '#ff2d1e', display: 'flex', gap: '4px' }}>
+        <span
+          style={{
+            ...commonStyles,
+            color: '#ff2d1e',
+          }}
+        >
           <ErrorIcon />
+          {status}
+        </span>
+      );
+    case 'CHANNEL_DISCONNECTED':
+      return (
+        <span
+          style={{
+            ...commonStyles,
+            color: '#ff2d1e',
+          }}
+        >
+          <DoNotDisturb />
           {status}
         </span>
       );
