d25410eb6ce7035f8383d8710efad61130bbd878
ChrisCoy <christopherleegs@outlook.com>
2023-09-05 20:10:31 -0300

    fix: fix qrcode

--- src/views/Painel/components/QrCodeView.tsx
+++ src/views/Painel/components/QrCodeView.tsx
@@ -38,8 +38,11 @@ export default function QrCodeView({ channelId }: QrCodeViewProps) {
       setIsLoading(true);
       try {
         const result = await api.get(`/channels/get-qrcode/${channelId}`);
+
+        console.log('qrcode', result);
+
         if (result.data.state != 'connected') {
-          setQrCode('data:image/webp;base64,' + result.data.qrCode);
+          setQrCode('data:image/webp;base64,' + result.data.base64);
           timer = setInterval(getStatus, statusRefreshTime);
         } else {
           setIsConnected(true);
